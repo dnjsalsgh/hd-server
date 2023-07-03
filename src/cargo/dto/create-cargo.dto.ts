2e2c55ddab69e12e5c8ef799dcbd3b92cfb6cc97
wmh <wmh@uvc.co.kr>
2023-07-03 15:48:17 +0900

    UPDATE(🖊️): cargo connect db

--- src/cargo/dto/create-cargo.dto.ts
+++ src/cargo/dto/create-cargo.dto.ts
@@ -1,1 +1,22 @@
-export class CreateCargoDto {}
+import { PickType } from '@nestjs/swagger';
+import { Cargo } from '../entities/cargo.entity';
+
+export class CreateCargoDto extends PickType(Cargo, [
+  'prefab',
+  'waterVolume',
+  'squareVolume',
+  'width',
+  'length',
+  'height',
+  'weight',
+  'orthopedic',
+  'barcode',
+  'destination',
+  'source',
+  'breakDown',
+  'piece',
+  'state',
+  'parent',
+  'modelPath',
+  'simulation',
+]) {}
