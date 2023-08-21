52cde8f16570f3ee55c3a0b742b32d3ec2a143ad
wmh <wmh@uvc.co.kr>
2023-08-21 12:24:12 +0900

    UPDATE(🖊️): basic schedule system

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  /**
+   * * * * * * *
+   * | | | | | |
+   * | | | | | day of week
+   * | | | | months
+   * | | | day of month
+   * | | hours
+   * | minutes
+   * seconds (optional)
+   */
+  // scheduler를 태스트 하기 위한 것
+  // @Cron('45 * * * * *')
+  // handleCron() {
+  //   console.log('Called when the current second is 45');
+  // }
 }
