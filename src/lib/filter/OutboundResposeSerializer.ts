ce2df954dc356b04f96c308b979bb78d7ea0239e
wmh <wmh@uvc.co.kr>
2023-08-31 17:39:24 +0900

    UPDATE(🖊️): make only message when mqtt publish message

---
+++ src/lib/filter/OutboundResposeSerializer.ts
@@ -0,0 +1,16 @@
+import { Serializer, OutgoingResponse } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+export class OutboundResponseSerializer implements Serializer {
+  private readonly logger = new Logger('OutboundResponseIdentitySerializer');
+
+  serialize(value: any): OutgoingResponse {
+    this.logger.debug(
+      `-->> Serializing outbound response: \n${JSON.stringify(value)}`,
+    );
+    this.logger.log(
+      `-->> Serializing outbound response: \n${JSON.stringify(value)}`,
+    );
+    return value.data;
+  }
+}
