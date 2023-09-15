08a30fe3af2d67471a72b5fa36534040438ad98b
wmh <wmh@uvc.co.kr>
2023-09-15 14:36:12 +0900

    FIX(🔨): change comment asrs-out-order.controller.ts

--- src/asrs-out-order/asrs-out-order.controller.ts
+++ src/asrs-out-order/asrs-out-order.controller.ts
@@ -8,7 +8,7 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AsrsOutOrderService } from './asrs-out-order.service';
 import { CreateAsrsOutOrderDto } from './dto/create-asrs-out-order.dto';
 import { UpdateAsrsOutOrderDto } from './dto/update-asrs-out-order.dto';
@@ -20,6 +20,12 @@ import { AsrsOutOrder } from './entities/asrs-out-order.entity';
 export class AsrsOutOrderController {
   constructor(private readonly asrsOutOrderService: AsrsOutOrderService) {}
 
+  @ApiOperation({
+    summary:
+      '[사용x] 자동창고(asrs) 에서 불출될 화물(awb)를 지정하는 작업지시, 패키지 시뮬레이션으로 등록되기 때문에 사용할 필요 없음',
+    description:
+      '[사용법] order: 불출순서, Asrs: 불출될 창고(랙), Awb: 목표 화물',
+  })
   @Post()
   create(@Body() createAsrsOutOrderDto: CreateAsrsOutOrderDto) {
     return this.asrsOutOrderService.create(createAsrsOutOrderDto);
