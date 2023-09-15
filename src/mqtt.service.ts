525c8cf236cf05fc58c7890ae9eea45967864c80
wmh <wmh@uvc.co.kr>
2023-09-15 10:17:36 +0900

    UPDATE(🖊️): implement check mqtt connection

--- src/mqtt.service.ts
+++ src/mqtt.service.ts
@@ -10,24 +10,9 @@ export class MqttService {
     this.mqttClient = mqtt.connect(
       `mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`,
     );
-
-    // // 연결 오류 처리
-    // this.mqttClient.on('error', (error) => {
-    //   Logger.error(`MQTT 연결 오류: ${error}`);
-    // });
   }
 
   getHello() {
-    // this.mqttClient = mqtt.connect(
-    //   `mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`,
-    // );
-    // this.mqttClient.on('error', (error) => {
-    //   Logger.error(`MQTT 연결 오류: ${error}`);
-    // });
-    // return 'Mqtt와 연결되어있습니다.';
-    // if (this.mqttClient.connected) {
-    //   return 'Mqtt와 연결되어있습니다.';
-    // }
     return this.mqttClient;
   }
 }
