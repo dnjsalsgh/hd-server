346f6563a08724bea77ca59f8947ef6cefc2cab8
wmh <wmh@uvc.co.kr>
2023-09-18 14:44:23 +0900

    UPDATE(🖊️): change parameter number to string when skidplatform-history insert

--- src/skid-platform-history/skid-platform-history.service.ts
+++ src/skid-platform-history/skid-platform-history.service.ts
@@ -4,6 +4,7 @@ import { UpdateSkidPlatformHistoryDto } from './dto/update-skid-platform-history
 import { InjectRepository } from '@nestjs/typeorm';
 import {
   Between,
+  DataSource,
   Equal,
   FindOperator,
   LessThanOrEqual,
@@ -11,9 +12,12 @@ import {
   Repository,
 } from 'typeorm';
 import { SkidPlatformHistory } from './entities/skid-platform-history.entity';
-import { AwbAttribute } from '../awb/entities/awb.entity';
-import { AsrsAttribute } from '../asrs/entities/asrs.entity';
-import { SkidPlatformAttribute } from '../skid-platform/entities/skid-platform.entity';
+import { Awb, AwbAttribute } from '../awb/entities/awb.entity';
+import { Asrs, AsrsAttribute } from '../asrs/entities/asrs.entity';
+import {
+  SkidPlatform,
+  SkidPlatformAttribute,
+} from '../skid-platform/entities/skid-platform.entity';
 import {
   AsrsOutOrder,
   AsrsOutOrderAttribute,
@@ -32,31 +36,59 @@ export class SkidPlatformHistoryService {
     @InjectRepository(AsrsOutOrder)
     private readonly asrsOutOrderRepository: Repository<AsrsOutOrder>,
     @Inject('MQTT_SERVICE') private client: ClientProxy,
+    private dataSource: DataSource,
   ) {}
   async create(createSkidPlatformHistoryDto: CreateSkidPlatformHistoryDto) {
-    const historyResult = await this.skidPlatformHistoryRepository.save(
-      createSkidPlatformHistoryDto,
-    );
+    if (
+      typeof createSkidPlatformHistoryDto.Asrs === 'string' &&
+      typeof createSkidPlatformHistoryDto.Awb === 'string' &&
+      typeof createSkidPlatformHistoryDto.SkidPlatform === 'string'
+    ) {
+      const AsrsResult = await this.dataSource.manager
+        .getRepository(Asrs)
+        .findOne({ where: { name: createSkidPlatformHistoryDto.Asrs } });
 
-    const historyResultObject = await this.skidPlatformHistoryRepository.find({
-      select: {
-        Awb: AwbAttribute,
-        Asrs: AsrsAttribute,
-        SkidPlatform: SkidPlatformAttribute,
-      },
-      relations: {
-        Awb: true,
-        Asrs: true,
-        SkidPlatform: true,
-      },
-    });
+      const AwbResult = await this.dataSource.manager
+        .getRepository(Awb)
+        .findOne({ where: { name: createSkidPlatformHistoryDto.Awb } });
 
-    // 현재 안착대에 어떤 화물이 들어왔는지 파악하기 위한 mqtt 전송
-    this.client
-      .send(`hyundai/skidPlatform/insert`, historyResultObject)
-      .pipe(take(1))
-      .subscribe();
-    return historyResult;
+      const SkidPlatformResult = await this.dataSource.manager
+        .getRepository(SkidPlatform)
+        .findOne({
+          where: { name: createSkidPlatformHistoryDto.SkidPlatform },
+        });
+
+      // 창고, 화물, 안착대의 이름으로 찾은 것들 id로 변환작업
+      createSkidPlatformHistoryDto.Asrs = AsrsResult.id;
+      createSkidPlatformHistoryDto.Awb = AwbResult.id;
+      createSkidPlatformHistoryDto.SkidPlatform = SkidPlatformResult.id;
+
+      const historyResult = await this.skidPlatformHistoryRepository.save(
+        createSkidPlatformHistoryDto as SkidPlatformHistory,
+      );
+
+      const historyResultObject = await this.skidPlatformHistoryRepository.find(
+        {
+          select: {
+            Awb: AwbAttribute,
+            Asrs: AsrsAttribute,
+            SkidPlatform: SkidPlatformAttribute,
+          },
+          relations: {
+            Awb: true,
+            Asrs: true,
+            SkidPlatform: true,
+          },
+        },
+      );
+
+      // 현재 안착대에 어떤 화물이 들어왔는지 파악하기 위한 mqtt 전송
+      this.client
+        .send(`hyundai/skidPlatform/insert`, historyResultObject)
+        .pipe(take(1))
+        .subscribe();
+      return historyResult;
+    }
   }
 
   async findAll(query: SkidPlatformHistory & BasicQueryParamDto) {
@@ -160,7 +192,7 @@ export class SkidPlatformHistoryService {
   ) {
     return this.skidPlatformHistoryRepository.update(
       id,
-      updateSkidPlatformHistoryDto,
+      updateSkidPlatformHistoryDto as SkidPlatformHistory,
     );
   }
 
@@ -171,111 +203,113 @@ export class SkidPlatformHistoryService {
   // plc의 데이터중 안착대 화물정보가 변경되었을 때 안착대 이력을 등록하기 위함입니다.
   async checkSkidPlatformChange(body: CreateSkidPlatformAndAsrsPlcDto) {
     // 안착대의 정보를 가져오기
-    const platInfo =
-      (body.Pallet_Rack1_Part_Info as unknown as {
-        skidPlatformId: number;
-        awbId: number;
-      }) ||
-      (body.Pallet_Rack2_Part_Info as unknown as {
-        skidPlatformId: number;
-        awbId: number;
-      }) ||
-      (body.Pallet_Rack3_Part_Info as unknown as {
-        skidPlatformId: number;
-        awbId: number;
-      }) ||
-      (body.Pallet_Rack4_Part_Info as unknown as {
-        skidPlatformId: number;
-        awbId: number;
-      });
-
-    // 어떤 안착대로 가야하는지
-    const skidPlatformId = platInfo.skidPlatformId;
-
-    // 어떤 창고에서 나왔는지
-    const asrsId = +body.LH_ASRS_ID || +body.RH_ASRS_ID;
-
-    // 어떤 화물인지
-    const awbInfo =
-      (body.ASRS_LH_Rack1_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack2_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack3_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack4_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack5_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack6_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack7_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack8_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      }) ||
-      (body.ASRS_LH_Rack9_Part_Info as unknown as {
-        awbId: number;
-        count: number;
-      });
-
-    // 화물정보 안에 화물Id 들어왔다고 가정
-    const awbId = awbInfo.awbId;
-    // 화물정보 안에 화물수량 들어왔다고 가정
-    const count = awbInfo.count;
-    // 화물이 인입인지 인출인지 확인
-    let inOutType = '';
-    if (body.Out_Conveyor_Start) {
-      // out 컨베이어 밸트가 움직일 때만 안착대에 입고되었다고 판단
-      inOutType = 'in';
-    } else {
-      inOutType = 'out';
-    }
-
-    //TODO 어느 창고에서 왔는지, 어떤 화물인지를 추적해서 작업지시를 가져옵니다.
-    const asrsOutOrderResult = await this.asrsOutOrderRepository.findOne({
-      where: { Asrs: asrsId, Awb: awbId },
-      order: { id: 'desc' },
-    });
-
-    if (!asrsOutOrderResult)
-      throw new NotFoundException('자동창고 작업지시를 찾지 못했습니다.');
-
-    const skidPlatformHistoryBody: CreateSkidPlatformHistoryDto = {
-      AsrsOutOrder: asrsOutOrderResult.id,
-      Asrs: asrsId,
-      SkidPlatform: skidPlatformId,
-      Awb: awbId,
-      inOutType: inOutType,
-      count: count,
-    };
-
-    // 입고, 출고에 따른 값 계산
-    const topLevelHistory = await this.skidPlatformHistoryRepository.findOne({
-      where: { Awb: awbId, Asrs: asrsId },
-      order: { createdAt: 'desc' },
-    });
-    if (skidPlatformHistoryBody.inOutType === 'in')
-      skidPlatformHistoryBody.count += topLevelHistory.count;
-    else if (skidPlatformHistoryBody.inOutType === 'out')
-      skidPlatformHistoryBody.count =
-        topLevelHistory.count - skidPlatformHistoryBody.count;
-
-    await this.skidPlatformHistoryRepository.save(skidPlatformHistoryBody);
+    //   const platInfo =
+    //     (body.Pallet_Rack1_Part_Info as unknown as {
+    //       skidPlatformId: number;
+    //       awbId: number;
+    //     }) ||
+    //     (body.Pallet_Rack2_Part_Info as unknown as {
+    //       skidPlatformId: number;
+    //       awbId: number;
+    //     }) ||
+    //     (body.Pallet_Rack3_Part_Info as unknown as {
+    //       skidPlatformId: number;
+    //       awbId: number;
+    //     }) ||
+    //     (body.Pallet_Rack4_Part_Info as unknown as {
+    //       skidPlatformId: number;
+    //       awbId: number;
+    //     });
+    //
+    //   // 어떤 안착대로 가야하는지
+    //   const skidPlatformId = platInfo.skidPlatformId;
+    //
+    //   // 어떤 창고에서 나왔는지
+    //   const asrsId = +body.LH_ASRS_ID || +body.RH_ASRS_ID;
+    //
+    //   // 어떤 화물인지
+    //   const awbInfo =
+    //     (body.ASRS_LH_Rack1_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack2_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack3_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack4_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack5_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack6_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack7_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack8_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     }) ||
+    //     (body.ASRS_LH_Rack9_Part_Info as unknown as {
+    //       awbId: number;
+    //       count: number;
+    //     });
+    //
+    //   // 화물정보 안에 화물Id 들어왔다고 가정
+    //   const awbId = awbInfo.awbId;
+    //   // 화물정보 안에 화물수량 들어왔다고 가정
+    //   const count = awbInfo.count;
+    //   // 화물이 인입인지 인출인지 확인
+    //   let inOutType = '';
+    //   if (body.Out_Conveyor_Start) {
+    //     // out 컨베이어 밸트가 움직일 때만 안착대에 입고되었다고 판단
+    //     inOutType = 'in';
+    //   } else {
+    //     inOutType = 'out';
+    //   }
+    //
+    //   //TODO 어느 창고에서 왔는지, 어떤 화물인지를 추적해서 작업지시를 가져옵니다.
+    //   const asrsOutOrderResult = await this.asrsOutOrderRepository.findOne({
+    //     where: { Asrs: asrsId, Awb: awbId },
+    //     order: { id: 'desc' },
+    //   });
+    //
+    //   if (!asrsOutOrderResult)
+    //     throw new NotFoundException('자동창고 작업지시를 찾지 못했습니다.');
+    //
+    //   const skidPlatformHistoryBody: CreateSkidPlatformHistoryDto = {
+    //     AsrsOutOrder: asrsOutOrderResult.id,
+    //     Asrs: asrsId,
+    //     SkidPlatform: skidPlatformId,
+    //     Awb: awbId,
+    //     inOutType: inOutType,
+    //     count: count,
+    //   };
+    //
+    //   // 입고, 출고에 따른 값 계산
+    //   const topLevelHistory = await this.skidPlatformHistoryRepository.findOne({
+    //     where: { Awb: awbId, Asrs: asrsId },
+    //     order: { createdAt: 'desc' },
+    //   });
+    //   if (skidPlatformHistoryBody.inOutType === 'in')
+    //     skidPlatformHistoryBody.count += topLevelHistory.count;
+    //   else if (skidPlatformHistoryBody.inOutType === 'out')
+    //     skidPlatformHistoryBody.count =
+    //       topLevelHistory.count - skidPlatformHistoryBody.count;
+    //
+    //   await this.skidPlatformHistoryRepository.save(
+    //     skidPlatformHistoryBody as SkidPlatformHistory,
+    //   );
   }
 }
