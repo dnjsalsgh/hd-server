80f785cfb8873e233d24d701db154072c2a7577d
wmh <wmh@uvc.co.kr>
2023-09-20 16:53:54 +0900

    REFACTORING(🖊️): make reset latest state asrs-history

--- src/asrs-history/asrs-history.service.ts
+++ src/asrs-history/asrs-history.service.ts
@@ -65,6 +65,27 @@ export class AsrsHistoryService {
     return asrsState.filter((v) => v.inOutType === 'in');
   }
 
+  /**
+   * 창고 이력에서 asrs_id를 기준으로 최신 안착대의 상태만 가져옴
+   */
+  async resetAsrs() {
+    const asrsState = await this.asrsHistoryRepository
+      .createQueryBuilder('ah')
+      .distinctOn(['ah.asrs_id'])
+      .leftJoinAndSelect('ah.Asrs', 'Asrs')
+      .leftJoinAndSelect('ah.Awb', 'Awb')
+      .leftJoinAndSelect('Awb.Scc', 'Scc') // awb의 Scc를 반환합니다.
+      // .where('ah.inOutType = :inOutType', { inOutType: 'in' }) // inOutType이 'in'인 경우 필터링
+      .orderBy('ah.asrs_id')
+      .addOrderBy('ah.id', 'DESC')
+      .getMany(); // 또는 getMany()를 사용하여 엔터티로 결과를 가져올 수 있습니다.
+
+    const currentState = asrsState.filter((v) => v.inOutType === 'in');
+    const ids = currentState.map((v) => v.id);
+    const deleteResult = await this.asrsHistoryRepository.delete(ids);
+    return deleteResult;
+  }
+
   async findAll(query: AsrsHistory & BasicQueryParamDto) {
     // createdAt 기간검색 처리
     const { createdAtFrom, createdAtTo } = query;
