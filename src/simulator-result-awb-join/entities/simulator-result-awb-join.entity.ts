06e887fed64c34fda2bdf93b39c5d81cdc60fb36
wmh <wmh@uvc.co.kr>
2023-10-12 11:46:41 +0900

    REFACTORING(🩹️): asrs plc data service

--- src/simulator-result-awb-join/entities/simulator-result-awb-join.entity.ts
+++ src/simulator-result-awb-join/entities/simulator-result-awb-join.entity.ts
@@ -10,26 +10,4 @@ export class SimulatorResultAwbJoin {
 
   @Column('int', { primary: true, name: 'awb_id', nullable: false })
   Awb: number;
-
-  // @PrimaryGeneratedColumn()
-  // id: number;
-  //
-  // @CreateDateColumn()
-  // createdAt: Date;
-  //
-  // @UpdateDateColumn()
-  // updatedAt: Date;
-  //
-  // @DeleteDateColumn()
-  // deletedAt: Date | null;
-  //
-  // @ManyToOne(
-  //   () => SimulatorResult,
-  //   (simulatorResult) => simulatorResult.simulatorResultAwbJoin,
-  //   { nullable: false },
-  // )
-  // SimulatorResult: Relation<SimulatorResult> | number;
-  //
-  // @ManyToOne(() => Awb, (awb) => awb.srJoin, { nullable: false })
-  // Awb: Relation<Awb> | number;
 }
