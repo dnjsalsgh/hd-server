505b116b9cbb5df24eebed56ceb70ce7fc0e8e9a
wmh <wmh@uvc.co.kr>
2023-11-01 20:33:16 +0900

    UPDATE(🖊️): add scc attribute in aircraft-schedule

--- src/aircraft-schedule/aircraft-schedule.service.ts
+++ src/aircraft-schedule/aircraft-schedule.service.ts
@@ -105,7 +105,7 @@ export class AircraftScheduleService {
           ...UldAttribute,
           uldHistories: {
             ...UldHistoryAttribute,
-            Awb: { ...AwbSimpleAttribute },
+            Awb: { ...AwbSimpleAttribute, Scc: SccAttribute },
           },
         },
       },
