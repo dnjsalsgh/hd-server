3de8d05f7777004bb2d8fc104440c0c1c9d114d5
wmh <wmh@uvc.co.kr>
2024-01-17 10:11:35 +0900

    update #91: create stack architecture in redisService

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -14,4 +14,19 @@ export class RedisService {
   async get(key: string) {
     return await this.redis.get(key);
   }
+
+  // Queue에 요소를 추가하는 메서드
+  async push(key: string, value: string) {
+    return await this.redis.rPush(key, value);
+  }
+
+  // Queue에서 요소를 제거하고 반환하는 메서드
+  async pop(key: string) {
+    return await this.redis.lPop(key);
+  }
+
+  // Queue의 크기를 확인하는 메서드
+  async size(key: string) {
+    return await this.redis.lLen(key);
+  }
 }
