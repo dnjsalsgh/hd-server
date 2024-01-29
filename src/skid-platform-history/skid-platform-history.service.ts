25530f5828793df946a173dc8e41cdf7b4307cca
wmh <wmh@uvc.co.kr>
2024-01-29 16:22:26 +0900

    asrs에서 mqtt 오는 정보 skidPlatform으로 mqtt 쏘는거로 변경

--- src/skid-platform-history/skid-platform-history.service.ts
+++ src/skid-platform-history/skid-platform-history.service.ts
@@ -468,10 +468,21 @@ export class SkidPlatformHistoryService {
       }
 
       // skidPlatformHistory를 mqtt에 보내기 위함
-      this.client
-        .send(`hyundai/skidPlatformHistory/insert`, skidPlatformHistoryFormIf)
-        .pipe(take(1))
-        .subscribe();
+      // this.client
+      //   .send(`hyundai/skidPlatformHistory/insert`, skidPlatformHistoryFormIf)
+      //   .pipe(take(1))
+      //   .subscribe();
+
+      // 현재 안착대에 어떤 화물이 들어왔는지 파악하기 위한 mqtt 전송 [작업지시 화면에서 필요함]
+      const skidPlatformNowState = await this.nowState();
+    this.client
+    .send(`hyundai/skidPlatform/insert`, {
+      statusCode: 200,
+      message: 'current skidPlatform state',
+      data: skidPlatformNowState,
+    })
+    .pipe(take(1))
+    .subscribe();
 
       await this.settingRedis(`p${SkidPlatformId}`, inOutType);
 
