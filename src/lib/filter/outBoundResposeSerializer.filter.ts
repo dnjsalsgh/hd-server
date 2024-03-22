dc89de7d61f1e83d0e01bf5d6a971666d0c3ad71
wmh <wmh@uvc.co.kr>
2024-03-22 13:41:49 +0900

    update #124: mqtt 로그 지우기

--- src/lib/filter/outBoundResposeSerializer.filter.ts
+++ src/lib/filter/outBoundResposeSerializer.filter.ts
@@ -6,9 +6,9 @@ export class OutboundResponseSerializer implements Serializer {
   private readonly logger = new Logger('OutboundResponseIdentitySerializer');
 
   serialize(value: any): OutgoingResponse {
-    this.logger.debug(
-      `-->> Serializing outbound response: \n${JSON.stringify(value)}`,
-    );
+    // this.logger.debug(
+    //   `-->> Serializing outbound response: \n${JSON.stringify(value)}`,
+    // );
     // this.logger.log(
     //   `-->> Serializing outbound response: \n${JSON.stringify(value)}`,
     // );
